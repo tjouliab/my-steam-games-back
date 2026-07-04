8203dc0c6636de1acfb576f677ec060214f6197a
Timothé Joulia-Béa <tim.jouliab@icloud.com>
2026-07-04 17:31:30 +0200

    feat: update game entity required and optionnal attributes

--- Data/Game/GameEntity.cs
+++ Data/Game/GameEntity.cs
@@ -6,12 +6,12 @@ public record GameEntity
     public required string Name { get; set; }
     public required bool IsVisible { get; set; } = true;
     public required string ImgIconUrl { get; set; }
-    public required int MetacriticScore { get; set; }
+    public int? MetacriticScore { get; set; }
     public required int PositiveReviews { get; set; }
     public required int NegativeReviews { get; set; }
     public required int PlayTime { get; set; }
     public required DateTime LastTimePlayed { get; set; }
-    public required DateTime? ReleaseDate { get; set; }
+    public required DateTime ReleaseDate { get; set; }
     public required int InitialPrice { get; set; }
     public IEnumerable<GenreEntity> Genres { get; set; } = [];
     public IEnumerable<TagEntity> Tags { get; set; } = [];
