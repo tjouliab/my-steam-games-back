4260d8431523a0007d170b82eec99cd61b08f618
Timothé Joulia-Béa <tim.jouliab@icloud.com>
2026-09-13 18:40:46 +0200

    fix: env create string parser to list

--- utils/types/env.ts
+++ utils/types/env.ts
@@ -2,13 +2,17 @@ import { apiKeySchema } from 'utils/types/api-key';
 import { z } from 'zod';
 import { playerIdSchema } from './player-id';
 
+const parseStringList = z.preprocess((value) => {
+  if (typeof value !== 'string') return value;
+  return JSON.parse(value);
+}, z.array(playerIdSchema));
+
 export const envSchema = z.object({
   PORT: z.coerce.number().default(3000),
   STEAM_API_KEY: apiKeySchema,
 
   PLAYER_ID: playerIdSchema,
-  FAMILY_PLAYER_ID: z.array(playerIdSchema),
-
+  FAMILY_PLAYERS_ID: parseStringList,
   DB_FILE_NAME: z.string(),
 });
 
