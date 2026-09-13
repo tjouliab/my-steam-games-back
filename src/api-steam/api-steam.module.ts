45cb446d9981bb2d74dc62f8f02904e574430b69
Timothé Joulia-Béa <tim.jouliab@icloud.com>
2026-09-13 16:37:05 +0200

    feat: add env variables

--- src/api-steam/api-steam.module.ts
+++ src/api-steam/api-steam.module.ts
@@ -1,7 +1,9 @@
+import { HttpModule } from '@nestjs/axios';
 import { Module } from '@nestjs/common';
 import { ApiSteamService } from './api-steam.service';
 
 @Module({
   providers: [ApiSteamService],
+  imports: [HttpModule],
 })
 export class ApiSteamModule {}
--- utils/types/env.ts
+++ utils/types/env.ts
@@ -1,9 +1,14 @@
 import { apiKeySchema } from 'utils/types/api-key';
 import { z } from 'zod';
+import { playerIdSchema } from './player-id';
 
 export const envSchema = z.object({
   PORT: z.coerce.number().default(3000),
   STEAM_API_KEY: apiKeySchema,
+
+  PLAYER_ID: playerIdSchema,
+  FAMILY_PLAYER_ID: z.array(playerIdSchema),
+
   DB_FILE_NAME: z.string(),
 });
 
