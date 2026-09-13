9e2443b02846a7762f8479b6cc565afdaecc5720
Timothé Joulia-Béa <tim.jouliab@icloud.com>
2026-09-13 16:45:38 +0200

    refactor: rewrite api steam service without rxjs

--- src/api-steam/api-steam.service.ts
+++ src/api-steam/api-steam.service.ts
@@ -1,15 +1,22 @@
 import { HttpService } from '@nestjs/axios';
 import { Injectable } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
+import { firstValueFrom } from 'rxjs';
 import { ApiKey } from 'utils/types/api-key';
 import { Env } from 'utils/types/env';
 import { GameId } from 'utils/types/game-id';
 import { PlayerId } from 'utils/types/player-id';
-import { GameDetailsReponse } from './dto/game-details.dto';
-import { GamesOwnedResponse } from './dto/game-owned.dto';
-import { GamesRecentlyPlayedResponse } from './dto/game-recently-played.dto';
-import { GameReviewResponse } from './dto/game-reviews.dto';
-import { PlayerAchievementsResponse } from './dto/player-achievements.dto';
+import { GameDetailsDto, GameDetailsReponse } from './dto/game-details.dto';
+import { GameOwnedDto, GamesOwnedResponse } from './dto/game-owned.dto';
+import {
+  GameRecentlyPlayedDto,
+  GamesRecentlyPlayedResponse,
+} from './dto/game-recently-played.dto';
+import { GameReviewDto, GameReviewResponse } from './dto/game-reviews.dto';
+import {
+  PlayerAchievementDto,
+  PlayerAchievementsResponse,
+} from './dto/player-achievements.dto';
 
 @Injectable()
 export class ApiSteamService {
@@ -25,53 +32,78 @@ export class ApiSteamService {
     this.apiKey = this.configService.get('STEAM_API_KEY', { infer: true });
   }
 
-  public getOwnedGames(playerId: PlayerId) {
-    return this.httpService.get<GamesOwnedResponse>(
-      `${this.apiUrl}/IPlayerService/GetOwnedGames/v0001/`,
-      {
-        params: {
-          key: this.apiKey,
-          steamid: playerId,
-          format: 'json',
-          include_appinfo: true,
-          include_played_free_games: true,
+  async getOwnedGames(playerId: PlayerId): Promise<GameOwnedDto[]> {
+    const response = await firstValueFrom(
+      this.httpService.get<GamesOwnedResponse>(
+        `${this.apiUrl}/IPlayerService/GetOwnedGames/v0001/`,
+        {
+          params: {
+            key: this.apiKey,
+            steamid: playerId,
+            format: 'json',
+            include_appinfo: true,
+            include_played_free_games: true,
+          },
         },
-      },
+      ),
     );
+
+    return response.data.games;
   }
 
-  public getRecentlyPlayedGames(playerId: PlayerId) {
-    return this.httpService.get<GamesRecentlyPlayedResponse>(
-      `${this.apiUrl}/IPlayerService/GetRecentlyPlayedGames/v0001/`,
-      { params: { key: this.apiKey, steamid: playerId } },
+  async getRecentlyPlayedGames(
+    playerId: PlayerId,
+  ): Promise<GameRecentlyPlayedDto[]> {
+    const response = await firstValueFrom(
+      this.httpService.get<GamesRecentlyPlayedResponse>(
+        `${this.apiUrl}/IPlayerService/GetRecentlyPlayedGames/v0001/`,
+        { params: { key: this.apiKey, steamid: playerId } },
+      ),
     );
+
+    return response.data.games;
   }
 
-  public getPlayerAchievements(playerId: PlayerId, gameId: GameId) {
-    return this.httpService.get<PlayerAchievementsResponse>(
-      `${this.apiUrl}/ISteamUserStats/GetPlayerAchievements/v0001/`,
-      { params: { key: this.apiKey, steamid: playerId, appid: gameId } },
+  async getPlayerAchievements(
+    playerId: PlayerId,
+    gameId: GameId,
+  ): Promise<PlayerAchievementDto[]> {
+    const response = await firstValueFrom(
+      this.httpService.get<PlayerAchievementsResponse>(
+        `${this.apiUrl}/ISteamUserStats/GetPlayerAchievements/v0001/`,
+        { params: { key: this.apiKey, steamid: playerId, appid: gameId } },
+      ),
     );
+
+    return response.data.achievements;
   }
 
-  public getGameDetails(gameId: GameId) {
-    return this.httpService.get<GameDetailsReponse>(
-      `${this.storeUrl}/api/appdetails`,
-      { params: { appIds: gameId } },
+  async getGameDetails(gameId: GameId): Promise<GameDetailsDto> {
+    const response = await firstValueFrom(
+      this.httpService.get<GameDetailsReponse>(
+        `${this.storeUrl}/api/appdetails`,
+        { params: { appIds: gameId } },
+      ),
     );
+
+    return response.data[gameId].data;
   }
 
-  public getGameReview(gameId: GameId) {
-    return this.httpService.get<GameReviewResponse>(
-      `${this.storeUrl}/appreviews/${gameId}`,
-      {
-        params: {
-          language: 'all',
-          purchase_type: 'all',
-          json: true,
-          num_per_page: 0,
+  async getGameReview(gameId: GameId): Promise<GameReviewDto> {
+    const response = await firstValueFrom(
+      this.httpService.get<GameReviewResponse>(
+        `${this.storeUrl}/appreviews/${gameId}`,
+        {
+          params: {
+            language: 'all',
+            purchase_type: 'all',
+            json: true,
+            num_per_page: 0,
+          },
         },
-      },
+      ),
     );
+
+    return response.data.gameReview;
   }
 }
