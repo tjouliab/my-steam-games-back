b8cd6d07d963d2970dc344f7e9b0feca200422f8
Timothé Joulia-Béa <tim.jouliab@icloud.com>
2026-09-21 22:40:38 +0200

    refactor: delete unused controller

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,14 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { ScheduleModule } from '@nestjs/schedule';
+import { DatabaseModule } from 'src/database/database.module';
 import { envSchema } from 'src/utils/types/env';
 import { ApiSteamModule } from './api-steam/api-steam.module';
-import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { GamesModule } from './games/games.module';
 import { PopulateJobItemModule } from './populate-job-item/populate-job-item.module';
 import { PopulateJobModule } from './populate-job/populate-job.module';
 import { TaskSchedulingModule } from './task-scheduling/task-scheduling.module';
-import { DatabaseModule } from 'src/database/database.module';
 
 @Module({
   imports: [
@@ -26,7 +25,6 @@ import { DatabaseModule } from 'src/database/database.module';
     PopulateJobItemModule,
     TaskSchedulingModule,
   ],
-  controllers: [AppController],
   providers: [AppService],
 })
 export class AppModule {}
